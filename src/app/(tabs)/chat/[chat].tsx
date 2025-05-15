5e87ba28816f99a5f564dda3b9a18885ea0e4141
anonymze <anodevfr@gmail.com>
2025-05-16 01:45:52 +0200

    trying tanstack db (not yet ready)

--- src/app/(tabs)/chat/[chat].tsx
+++ src/app/(tabs)/chat/[chat].tsx
@@ -44,177 +44,7 @@ export default function Page() {
 	const translateY = useSharedValue(0);
 	const translateX = useSharedValue(0);
 	const opacity = useSharedValue(1);
-
-	const mutateMessages = React.useCallback(
-		async (newMessage: MessageOptimistic) => {
-			// cancel any outgoing refetches
-			// (so they don't overwrite our optimistic update)
-			await queryClient.cancelQueries({ queryKey: ["messages", chatId, maxMessages] });
-
-			// snapshot the previous value
-			const previousMessages = queryClient.getQueryData(["messages", chatId, maxMessages]);
-
-			if (newMessage.file) {
-				const newMessages = newMessage.file.map((file) => ({
-					...newMessage,
-					id: `${Date.now()}_${Math.random().toString(36).slice(2, 7)}`,
-					file: file,
-				}));
-
-				// optimistically update to the new value
-				queryClient.setQueryData(["messages", chatId, maxMessages], (old: Message[]) => {
-					return [...newMessages, ...old];
-				});
-			} else {
-				// optimistically update to the new value
-				queryClient.setQueryData(["messages", chatId, maxMessages], (old: Message[]) => {
-					return [newMessage, ...old];
-				});
-			}
-
-			// return old messages before optimistic update for the context in onError
-			return previousMessages;
-		},
-		[chatId, maxMessages],
-	);
-
-	const mutationMessages = useMutation({
-		mutationFn: createMessageQuery,
-		// when mutate is called:
-		onMutate: mutateMessages,
-		// if the mutation fails,
-		// use the context returned from onMutate to roll back
-		onError: (err, newMessage, context) => {
-			Alert.alert(i18n[languageCode]("ERROR_GENERIC_PART1"), err.message);
-			queryClient.setQueryData(["messages", chatId, maxMessages], context);
-		},
-		// always refetch after error or success:
-		onSettled: () => {
-			// if you are on settled the mutation is still in pending status
-			// so we check if we have more than 1 mutation then we don't invalidate the query
-			const pendingMutations = queryClient
-				.getMutationCache()
-				.getAll()
-				.filter((mutation) => mutation.state.status === "pending");
-
-			if (pendingMutations.length > 1) return;
-			queryClient.invalidateQueries({ queryKey: ["messages", chatId, maxMessages] });
-		},
-	});
-
-	const mutationMessagesFile = useMutation({
-		mutationFn: createMessageWithFilesQuery,
-		onMutate: mutateMessages,
-		onError: (err, newMessage, context) => {
-			Alert.alert(i18n[languageCode]("ERROR_GENERIC_PART1"), err.message);
-			queryClient.setQueryData(["messages", chatId, maxMessages], context);
-		},
-		onSettled: () => {
-			const pendingMutations = queryClient
-				.getMutationCache()
-				.getAll()
-				.filter((mutation) => mutation.state.status === "pending");
-
-			if (pendingMutations.length > 1) return;
-			queryClient.invalidateQueries({ queryKey: ["messages", chatId, maxMessages] });
-		},
-	});
-
-	const { data: messages, isLoading: loadingMessages } = useQuery({
-		queryKey: ["messages", chatId, maxMessages],
-		queryFn: getMessagesQuery,
-		placeholderData: (prev) => prev,
-		structuralSharing: false,
-		refetchInterval: () => {
-			// pause refetching while a message is being sent
-			if (mutationMessages.isPending || mutationMessagesFile.isPending) return false;
-			return 6000;
-		},
-		select: (data) => {
-			return data;
-		},
-	});
-
-	const formSchema = React.useMemo(
-		() =>
-			z.object({
-				// prevent only white spaces
-				message: z.string().regex(/.*\S.*/),
-			}),
-		[],
-	);
-
-	const form = useForm({
-		defaultValues: {
-			message: "",
-		},
-		validators: {
-			onSubmit: formSchema,
-		},
-		onSubmit: ({ value }) => {
-			translateX.value = withSequence(
-				withTiming(-5, {
-					duration: 300,
-					easing: Easing.bezier(0.25, 0.1, 0.25, 1),
-				}),
-				// cubic
-				withTiming(75, { easing: Easing.bezier(0.25, 0.1, 0.25, 1) }),
-			);
-			translateY.value = withSequence(
-				withTiming(5, {
-					duration: 300,
-					easing: Easing.bezier(0.25, 0.1, 0.25, 1),
-				}),
-				// ease
-				withTiming(-75, { easing: Easing.bezier(0.25, 0.1, 0.25, 1) }),
-			);
-			opacity.value = withTiming(0, { easing: Easing.bezier(0, 0.1, 0, 0.1), duration: 600 }, (finished) => {
-				if (finished) {
-					translateY.value = 0;
-					translateX.value = 0;
-					opacity.value = withTiming(1, { duration: 350 });
-				}
-			});
-
-			form.reset();
-
-			// we have to set an id otherwise the list will not have a key extractor, and a date to show
-			mutationMessages.mutate({
-				id: `${Date.now()}_${Math.random().toString(36).slice(2, 7)}`,
-				app_user: appUser.user,
-				chat_room: chatId,
-				message: value.message,
-				createdAt: new Date().toISOString(),
-				// we flag it to show it as a pending message
-				optimistic: true,
-			});
-		},
-	});
-
-	const pickImage = React.useCallback(async () => {
-		let result = await ImagePicker.launchImageLibraryAsync({
-			mediaTypes: ["images", "videos"],
-			allowsEditing: false,
-			aspect: [1, 1],
-			quality: 0.4,
-			allowsMultipleSelection: true,
-			presentationStyle: UIImagePickerPresentationStyle.POPOVER,
-		});
-
-		if (result.canceled) return;
-
-		mutationMessagesFile.mutate({
-			// id is set later (to have differents ids for each file)
-			id: "",
-			app_user: appUser.user,
-			chat_room: chatId,
-			// following the order of the files selected
-			file: result.assets.reverse(),
-			createdAt: new Date().toISOString(),
-			// we flag it to show it as a pending message
-			optimistic: true,
-		});
-	}, []);
+	const messages: Message[] = [];
 
 	const animatedStyle = useAnimatedStyle(() => {
 		const spacing = 12;
@@ -225,12 +55,9 @@ export default function Page() {
 		};
 	});
 
-	const handleSubmit = React.useCallback(() => {
-		// delay to the next frame to avoid autocorrect messing up
-		requestAnimationFrame(() => {
-			form.handleSubmit();
-		});
-	}, [form]);
+	const handleSubmit =(value: string) => {
+		console.log("handleSubmit", value);
+	};
 
 	return (
 		<BackgroundLayout className="px-6">
@@ -286,37 +113,26 @@ export default function Page() {
 						/>
 					) : (
 						<View className="flex-1 items-center justify-center">
-							{loadingMessages ? (
-								<Text className="text-sm text-defaultGray">Chargement...</Text>
-							) : (
-								<Text className="text-sm text-defaultGray">Pas de message</Text>
-							)}
+							<Text className="text-sm text-defaultGray">Pas de message</Text>
 						</View>
 					)}
 
 					<View className="flex-row items-center gap-0.5">
 						<View className={cn("mb-3 flex-shrink flex-row items-center rounded-xl border border-lightGray")}>
-							<form.Field name="message">
-								{(field) => (
-									<TextInput
-										placeholderTextColor={config.theme.extend.colors.lightGray}
-										returnKeyType="default"
-										autoCapitalize="none"
-										keyboardType="default"
-										submitBehavior="newline"
-										multiline={true}
-										placeholder={`${i18n[languageCode]("MESSAGE")}...`}
-										className="flex-1 p-3 pr-0"
-										onChangeText={field.handleChange}
-										defaultValue={field.state.value}
-									/>
-								)}
-							</form.Field>
-							<Actions pickImage={pickImage} />
+							<TextInput
+								placeholderTextColor={config.theme.extend.colors.lightGray}
+								returnKeyType="default"
+								autoCapitalize="none"
+								keyboardType="default"
+								submitBehavior="newline"
+								multiline={true}
+								placeholder={`${i18n[languageCode]("MESSAGE")}...`}
+								className="flex-1 p-3 pr-0"
+							/>
 						</View>
 						<SendButton
-							handleSubmit={handleSubmit}
-							loadingMessages={loadingMessages}
+							handleSubmit={() => handleSubmit("TEST")}
+							loadingMessages={false}
 							opacity={opacity}
 							translateX={translateX}
 							translateY={translateY}
@@ -359,7 +175,7 @@ const SendButton = React.memo(
 					opacity: loadingMessages ? 0.5 : opacity,
 					transform: [{ translateX }, { translateY }],
 				}}
-				className={cn("p-1.5 pr-0 mb-3")}
+				className={cn("mb-3 p-1.5 pr-0")}
 			>
 				<SendIcon size={20} color={config.theme.extend.colors.primaryLight} />
 			</AnimatedPressable>
