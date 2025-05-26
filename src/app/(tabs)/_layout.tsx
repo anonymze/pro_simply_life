d75106dbb0986cf71aad2668006b109ba06b47ef
anonymze <anodevfr@gmail.com>
2025-05-26 21:40:56 +0200

    fix: android

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -83,6 +83,13 @@ export default function AppLayout() {
 					}}
 				/>
 				<Tabs.Screen
+					name="bracket"
+					options={{
+						header: () => <HeaderLayout title="Bracket" />,
+						href: null,
+					}}
+				/>
+				<Tabs.Screen
 					name="chat"
 					options={{
 						title: "Messages",
