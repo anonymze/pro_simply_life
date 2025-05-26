7adc8cedb090bc4cde40f54e3b9658ebbe7cccd7
anonymze <anodevfr@gmail.com>
2025-05-26 14:27:26 +0200

    add: signature

--- src/components/carousel.tsx
+++ src/components/carousel.tsx
@@ -2,12 +2,11 @@ import { Dimensions, NativeScrollEvent, NativeSyntheticEvent, ScrollView, View }
 import Animated, { useAnimatedStyle, withSpring } from "react-native-reanimated";
 import React from "react";
 
-import CardEvent from "./card/card-event";
-
 
 const screenWidth = Dimensions.get("window").width;
 const horizontalPadding = 38;
 const cardWidth = screenWidth - horizontalPadding;
+const gap = 16;
 
 export default function Carousel<T>({ data, children }: { data: T[]; children: (data: T[], cardWidth: number) => React.ReactNode }) {
 	const [currentIndex, setCurrentIndex] = React.useState(0);
@@ -31,13 +30,13 @@ export default function Carousel<T>({ data, children }: { data: T[]; children: (
 				decelerationRate={0.1}
 				snapToOffsets={data.map((_, idx) => {
 					if (idx === 0) return 0;
-					return cardWidth * idx - 4 * idx;
+					return cardWidth * idx;
 				})}
-				contentContainerStyle={{ paddingRight: 42, gap: 16 }} // keep right padding for last card
+				contentContainerStyle={{ paddingRight: gap * 2, gap }} // keep right padding for last card
 				scrollEventThrottle={undefined}
 				style={{ flexGrow: 0 }}
 			>
-				{children(data, cardWidth)}
+				{children(data, cardWidth - gap)}
 			</ScrollView>
 			<View className="mt-4 flex-row items-center gap-2">
 				{data.map((_, idx) => (
