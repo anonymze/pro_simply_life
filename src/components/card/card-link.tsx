d14e8e3dfbb6f2701b0f461398344b8d1ee33124
anonymze <anodevfr@gmail.com>
2025-05-31 01:51:31 +0200

    add: fix primary colors

--- src/components/card/card-link.tsx
+++ src/components/card/card-link.tsx
@@ -5,6 +5,8 @@ import { cn } from "@/utils/cn";
 import React from "react";
 
 
+const windowWidth = Dimensions.get("window").width;
+
 export default function CardLinkTablet({
 	icon,
 	title,
@@ -22,7 +24,7 @@ export default function CardLinkTablet({
 		<Link href={link} push asChild>
 			<TouchableOpacity className="rounded-2xl gap-1 items-center" hitSlop={5}>
 				<View className={cn("size-24 items-center justify-center  rounded-2xl bg-primaryUltraLight", backgroundIcon)}>{icon}</View>
-				<Text className="font-semibold text-primary mt-2 ">{Dimensions.get("window").width}</Text>
+				<Text className={cn("font-semibold text-primary mt-2", windowWidth < 380 && "text-[0.89rem]")}>{truncateText(title, 12)}</Text>
 				{/* <Text className="text font-medium text-lightGray text-center">{truncateText(description, 10)}</Text> */}
 			</TouchableOpacity>
 		</Link>
