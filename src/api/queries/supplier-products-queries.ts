53724fbb0de8f120d6a9fd1a438c9f66ce3adcfc
anonymze <anodevfr@gmail.com>
2025-07-07 12:11:56 +0200

    fix: knip

--- src/api/queries/supplier-products-queries.ts
+++ src/api/queries/supplier-products-queries.ts
@@ -1,15 +1,13 @@
-import { PaginatedResponse } from "@/types/response";
 import { SupplierProduct } from "@/types/supplier";
 import { QueryKey } from "@tanstack/react-query";
 
 import { api } from "../_config";
 
-
-export async function getSupplierProductsQuery({ queryKey }: { queryKey: QueryKey }) {
-	const [, filters] = queryKey;
-	const response = await api.get<PaginatedResponse<SupplierProduct>>("/api/supplier-products", { params: filters });
-	return response.data;
-}
+// export async function getSupplierProductsQuery({ queryKey }: { queryKey: QueryKey }) {
+// 	const [, filters] = queryKey;
+// 	const response = await api.get<PaginatedResponse<SupplierProduct>>("/api/supplier-products", { params: filters });
+// 	return response.data;
+// }
 
 export async function getSupplierProductQuery({ queryKey }: { queryKey: QueryKey }) {
 	const [, supplierProductId] = queryKey;
