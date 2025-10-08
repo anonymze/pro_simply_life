3098edc55d289c4a061c620283e93b6bd7dde715
anonymze <anodevfr@gmail.com>
2025-10-09 01:40:53 +0200

    add: test git reflog

--- src/utils/register-push-notifications.ts
+++ src/utils/register-push-notifications.ts
@@ -2,8 +2,6 @@ import * as Notifications from "expo-notifications";
 import { Alert, Platform } from "react-native";
 import Constants from "expo-constants";
 import * as Device from "expo-device";
-import * as Notifications from "expo-notifications";
-import { Platform } from "react-native";
 import config from "tailwind.config";
 
 
