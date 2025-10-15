bdd76d14bba7b02a6b5c994e8b6432955758f14c
anonymze <anodevfr@gmail.com>
2025-10-15 14:45:30 +0200

    add: private equity

--- src/app/(tabs)/sports/index.tsx
+++ src/app/(tabs)/sports/index.tsx
@@ -4,62 +4,126 @@ import Title from "@/components/ui/title";
 import BackgroundLayout from "@/layouts/background-layout";
 import { Sport } from "@/types/sport";
 import { USER_DEV_ID, USER_LEONIE_ID, USER_MATHIEU_ID } from "@/utils/helper";
-import { withQueryWrapper } from "@/utils/libs/react-query";
 import { getStorageUserInfos } from "@/utils/store";
+import { useQuery } from "@tanstack/react-query";
 import { Link } from "expo-router";
 import * as WebBrowser from "expo-web-browser";
 import { ArrowRightIcon, FileSpreadsheetIcon } from "lucide-react-native";
 import React from "react";
-import { Text, TouchableOpacity, View } from "react-native";
+import { ActivityIndicator, Text, TouchableOpacity, View } from "react-native";
 import config from "tailwind.config";
+
+// export default function Page() {
+// 	const appUser = getStorageUserInfos();
+// 	return withQueryWrapper(
+// 		{
+// 			queryKey: ["sports"],
+// 			queryFn: getSportsQuery,
+// 		},
+// 		({ data }) => {
+// 			return (
+// 				<BackgroundLayout className="pt-safe px-4">
+// 					{appUser?.user.id === USER_LEONIE_ID ||
+// 					appUser?.user.id === USER_MATHIEU_ID ||
+// 					appUser?.user.id === USER_DEV_ID ? (
+// 						<View className="flex-row items-center justify-between">
+// 							<Title title="Sport et Patrimoine" />
+// 							<TouchableOpacity
+// 								className="p-3"
+// 								hitSlop={5}
+// 								onPress={async () => {
+// 									const webUrl =
+// 										"https://www.notion.so/2712a4ea516080558c9efd6591efc4f5?v=2712a4ea516080f98116000c3a3eedb1&source=copy_link";
+// 									await WebBrowser.openBrowserAsync(webUrl);
+// 								}}
+// 							>
+// 								<View className="size-10 items-center justify-center rounded-full bg-white shadow-sm shadow-defaultGray/20">
+// 									<FileSpreadsheetIcon size={22} color={config.theme.extend.colors.primary} />
+// 								</View>
+// 							</TouchableOpacity>
+// 						</View>
+// 					) : (
+// 						<Title title="Sport et Patrimoine" className="mb-6" />
+// 					)}
+
+// 					<Card
+// 						sports={data.docs.filter((item) => item.category === "fiscal")}
+// 						category={"fiscal"}
+// 						categoryLabel={"Fiscalité"}
+// 					/>
+// 					<Card
+// 						sports={data.docs.filter((item) => item.category === "international")}
+// 						category={"international"}
+// 						categoryLabel={"International"}
+// 					/>
+// 				</BackgroundLayout>
+// 			);
+// 		},
+// 	)();
+// }
 
 export default function Page() {
 	const appUser = getStorageUserInfos();
-	return withQueryWrapper(
-		{
-			queryKey: ["sports"],
-			queryFn: getSportsQuery,
-		},
-		({ data }) => {
-			return (
-				<BackgroundLayout className="pt-safe px-4">
-					{appUser?.user.id === USER_LEONIE_ID ||
-					appUser?.user.id === USER_MATHIEU_ID ||
-					appUser?.user.id === USER_DEV_ID ? (
-						<View className="flex-row items-center justify-between">
-							<Title title="Sport et Patrimoine" />
-							<TouchableOpacity
-								className="p-3"
-								hitSlop={5}
-								onPress={async () => {
-									const webUrl =
-										"https://www.notion.so/2712a4ea516080558c9efd6591efc4f5?v=2712a4ea516080f98116000c3a3eedb1&source=copy_link";
-									await WebBrowser.openBrowserAsync(webUrl);
-								}}
-							>
-								<View className="size-10 items-center justify-center rounded-full bg-white shadow-sm shadow-defaultGray/20">
-									<FileSpreadsheetIcon size={22} color={config.theme.extend.colors.primary} />
-								</View>
-							</TouchableOpacity>
-						</View>
-					) : (
-						<Title title="Sport et Patrimoine" className="mb-6" />
-					)}
+	// const { data, isLoading, isError } = useQuery({
+	// 	queryKey: ["sports"],
+	// 	queryFn: getSportsQuery,
+	// });
 
-					<Card
-						sports={data.docs.filter((item) => item.category === "fiscal")}
-						category={"fiscal"}
-						categoryLabel={"Fiscalité"}
-					/>
-					<Card
-						sports={data.docs.filter((item) => item.category === "international")}
-						category={"international"}
-						categoryLabel={"International"}
-					/>
-				</BackgroundLayout>
-			);
-		},
-	)();
+	// if (isLoading) {
+	// 	return (
+	// 		<ActivityIndicator
+	// 			className="absolute bottom-0 left-0 right-0 top-0"
+	// 			size="large"
+	// 			color={config.theme.extend.colors.primary}
+	// 		/>
+	// 	);
+	// }
+
+	// if (!data || isError) {
+	// 	return (
+	// 		<View className="flex-1 items-center justify-center">
+	// 			<Text className="text-sm text-defaultGray">Erreur</Text>
+	// 		</View>
+	// 	);
+	// }
+
+	return (
+		<BackgroundLayout className="pt-safe px-4">
+			{appUser?.user.id === USER_LEONIE_ID ||
+			appUser?.user.id === USER_MATHIEU_ID ||
+			appUser?.user.id === USER_DEV_ID ? (
+				<View className="flex-row items-center justify-between">
+					<Title title="Sport et Patrimoine" />
+					<TouchableOpacity
+						className="p-3"
+						hitSlop={5}
+						onPress={async () => {
+							const webUrl =
+								"https://www.notion.so/2712a4ea516080558c9efd6591efc4f5?v=2712a4ea516080f98116000c3a3eedb1&source=copy_link";
+							await WebBrowser.openBrowserAsync(webUrl);
+						}}
+					>
+						<View className="size-10 items-center justify-center rounded-full bg-white shadow-sm shadow-defaultGray/20">
+							<FileSpreadsheetIcon size={22} color={config.theme.extend.colors.primary} />
+						</View>
+					</TouchableOpacity>
+				</View>
+			) : (
+				<Title title="Sport et Patrimoine" className="mb-6" />
+			)}
+
+			{/*<Card
+				sports={data.docs.filter((item) => item.category === "fiscal")}
+				category={"fiscal"}
+				categoryLabel={"Fiscalité"}
+			/>
+			<Card
+				sports={data.docs.filter((item) => item.category === "international")}
+				category={"international"}
+				categoryLabel={"International"}
+			/>*/}
+		</BackgroundLayout>
+	);
 }
 
 const Card = ({
