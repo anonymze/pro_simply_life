763e000d9b5fe73e1d1cb5eb692e1512d1b56755
anonymze <anodevfr@gmail.com>
2025-12-03 14:50:29 +0100

    add: all updtes

--- src/types/supplier.ts
+++ src/types/supplier.ts
@@ -24,6 +24,17 @@ export interface Supplier {
 		password?: string | null;
 		remarques?: string | null;
 	};
+	pea?: {
+		banque?: string | null;
+		title_vif?: ("yes" | "no") | null;
+		architecture_open?: ("yes" | "no") | null;
+		fonds?: string | null;
+		vp?: ("yes" | "no") | null;
+		retrocession_gestion_libre?: string | null;
+		retrocession_gestion_mandat?: string | null;
+		passage_order?: string | null;
+		interface?: string | null;
+	};
 	enveloppe?: {
 		global?: number | null;
 		amount?: number | null;
